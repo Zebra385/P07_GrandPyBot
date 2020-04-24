1e0f5823a23c493ac0327040bc150cf6a7f515d1
Zebra385 <houcheserge@gmail.com>
2020-04-24 15:10:34 +0200

    utilisation ajax

--- static/js/affichage.js
+++ static/js/affichage.js
@@ -1,16 +1,29 @@
 
+var request = new XMLHttpRequest();
+request.onreadystatechange = function() {
+    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+        var response = JSON.parse(this.responseText);
+
+        alert ('ladresse du site : ' + site +' est : ' + response.candidates.formatted_address);
+
+    }
+};
+
+
 var $f = $('#question');
 
-
-var $r = $('#result');
-
-
-
-
 $f.on('submit', function(e){
+    var $r = $('#result');
     e.preventDefault();
-    $r.text('changement');
-    var id_article = $(this).attr("name");
-    alert(id_article);
+
+    alert('changement');
+    var question = $('#champ').val();
+    alert('tu recherche l adresse du site: ' + question );
+    var site = question.split(" ")[10];
+
+    $r.text('tu recherche l adresse du site: ' + site );
+    var package_url = 'https://www.google.fr/maps/place/openclassrooms}';
+    request.open("GET", 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input={site}&inputtype=textquery&fields=formatted_address,name,geometry&key=AIzaSyAu-GCUJE1l_rVUxe0Tk0c5DXdNXnM94Oo');
+    request.send();
 
 });
