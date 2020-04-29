f17cfab020bb2ce1c0f93fbbb2f958715cc734d9
Zebra385 <houcheserge@gmail.com>
2020-04-29 18:31:57 +0200

    affichage carte du site

--- static/js/affichage.js
+++ static/js/affichage.js
@@ -7,9 +7,19 @@ $(function(){
         e.preventDefault();
         var question = $('#champ').val();
         var $r = $('#result');
+        var $r1 = $('#result1');
+        var $r2 = $('#result2');
+
+
+
         alert('tu recherche l adresse du site: ' + question );
         var question_json= {'text':question,};
         alert('en json' + question_json.text);
+        question = question +"\n"
+        $r.text(question);
+
+
+
         if (question !=""){
             $.ajax({
             url: '/find-site',
@@ -22,8 +32,22 @@ $(function(){
             success: function(data){
                 alert('success' );
                     //alert(ata[di].candidates[0].formatted_address);
-                    $r.text('l adresse du site est : ' +data.candidates[0].formatted_address);
-                    //$r.text= data[i].candidates[0].formatted_address;
+                    var lieu = data.candidates[0].name;
+                    var adresse = data.candidates[0].formatted_address;
+
+                    $r1.text('Bien sûr mon poussin ! La voici l adresse de '+ lieu + ': ' + adresse);
+
+                    $r2.text(' Et je suis si gentils que je  l indique sur la carte ci dessous : ');
+                    // on va inserer la carte
+
+                    var img = document.createElement("img");
+                    var center = 'center='+adresse;
+                    var markers ='&markers=size:mid%7Ccolor:red%7CSan'+adresse;
+                    img.src = 'https://maps.googleapis.com/maps/api/staticmap?'+ center + '&zoom=12&size=600x300&maptype=roadmap' + markers +'&key=AIzaSyAu-GCUJE1l_rVUxe0Tk0c5DXdNXnM94Oo';
+
+
+                   $('#map').replaceWith(img);
+
                     },
             error: function(){
                  alert('erreur recup data');
