7eaafebe62f941eb5249a32d8a099e12eda9bd67
Zebra385 <houcheserge@gmail.com>
2020-05-06 11:16:16 +0200

    ajout icone d'attente

--- static/js/affichage.js
+++ static/js/affichage.js
@@ -6,18 +6,16 @@ $(function()
 
 
     var $f = $('#question');
+    //$r.val(" ");
     $f.on('click', function(e){
-        with($(this)) {
-              button('loading');
-              setTimeout(function () {
-                button('reset');
-              }, 4000);
-            }
         e.preventDefault();
+        var $r = $('#champ_question_reponse');
+        $f.hide();// effce le bouton envoyer
+        $('#question_chargement').show(); // affiche le bouton télechargement
 
 
         var question = $('#champ').val();
-        var $r = $('#champ_question_reponse');
+
         //var $r1 = $('#result1');
         //var $r2 = $('#result2');
         //var $r3 = $('#result3');
@@ -27,9 +25,7 @@ $(function()
         var question_json= {'text':question,};
 
         if (question !=""){
-            setTimeout(function() {
-                console.log("I'm here!")
-                }, 5000);
+
             $.ajax({
             url: '/find-site',
             method: 'POST',
@@ -38,45 +34,21 @@ $(function()
             },
             dataType: 'json',
             data: JSON.stringify(question_json),
-            success: function(data){
+            success: async function recover_adresse(data){
                     var lieu = data.candidates[0].name;
                     var adresse = data.candidates[0].formatted_address;
-
                     var site_json ={'site':adresse,}
-
-                    setTimeout(function() {
-                        $r.append('Bien sûr mon poussin ! La voici l\' adresse de '+ lieu + ': ' + adresse);
-                         $r.append('\r\n');
-
-
-
-                        }, 2000);
-                     setTimeout(function() {
-                        $r.append(' Et je suis si gentils que je  l\' indique sur la carte ci dessous : ');
-                         $r.append('\r\n');
-                          }, 4000);
-                      setTimeout(function() {
-                           $r.append('Mais t ai-je déjà raconté l\' histoire de ce quartier qui m a vu en culottes courtes ?');
-                         $r.append('\r\n');
-
-                           }, 5000);
-
-
-                    //$r1.text('Bien sûr mon poussin ! La voici l adresse de '+ lieu + ': ' + adresse;
-                    //$r2.text(' Et je suis si gentils que je  l indique sur la carte ci dessous : ');)
-                    //$r3.text('Mais t ai-je déjà raconté l histoire de ce quartier qui m a vu en culottes courtes ?');
-                    // on va inserer la carte
-
+                    $r.append('Bien sûr mon poussin ! La voici l\' adresse de '+ lieu + ': ' + adresse);
+                    $r.append('\r\n');
+                    $r.append(' Et je suis si gentils que je  l\' indique sur la carte ci dessous : ');
+                    $r.append('\r\n');
+                    $r.append('Mais t ai-je déjà raconté l\' histoire de ce quartier qui m\' a vu en culottes courtes ?');
+                    $r.append('\r\n');
                     var img = document.createElement("img");
                     var center = 'center='+adresse;
                     var markers ='&markers=size:mid%7Ccolor:red%7CSan'+adresse;
                     img.src = 'https://maps.googleapis.com/maps/api/staticmap?'+ center + '&zoom=12&size=400x200&maptype=roadmap' + markers +'&key=AIzaSyAu-GCUJE1l_rVUxe0Tk0c5DXdNXnM94Oo';
-                    setTimeout(function() {
-
-                           $('#map').replaceWith(img);
-                           }, 4500);
-
-
+                    $('#map').replaceWith(img);
                     if (adresse != "")
 
                                 {
@@ -88,9 +60,7 @@ $(function()
                                           },
                                 dataType: 'text',
                                 data: JSON.stringify(site_json),
-                                success: function(data){
-
-
+                                success: async function recover_article(data){
                                         var article = data
                                         $r.append(article);
                                                         },
@@ -98,21 +68,23 @@ $(function()
                                     alert('erreur recup article');
                                                 }
                                         });
-
                                 }
                     },
             error: function(){
                  alert('erreur recup data1');
             }
             });
-        }
+            }
+        setTimeout(function(){
+        $('#question_chargement').hide();// effce le bouton chargement
+        $f.show();   // affiche le bouton envoyer
+        },2000);
+
+
+
+});
+})
 
 
 
 
-
-
-
-    });
-
-    })
