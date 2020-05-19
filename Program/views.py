360d8dbd4d36d2d98a065d20f4578685672a65a0
Zebra385 <houcheserge@gmail.com>
2020-05-19 11:26:57 +0200

    rajout route pour map pour securisation

--- Program/views.py
+++ Program/views.py
@@ -5,9 +5,7 @@ import os
 
 app = Flask(__name__)
 
-# Config options - Make sure you created a 'config.py' file.
-app.config.from_object('config')
-# To get one variable, tape app.config['MY_VARIABLE']
+
 
 @app.route('/')
 @app.route('/GrandPyBot')
